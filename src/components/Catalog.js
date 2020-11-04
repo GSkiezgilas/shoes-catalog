03b1d1e202fa8d4be58f10b600e9790ba0ef61b8
GSkiezgilas <giedriusskiezgilas@yahoo.com>
2020-11-05 01:44:57 +0200

    add sort and select features, fix search feature

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,58 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import ProductCard from "./Product";
 import { PRODUCTS } from '../data/data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './Catalog.scss';
-
 
 const Catalog = () => {
   const [products, setProducts] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [sortType, setSortType] = useState('unsorted');
+  const [searchedProducts, setSearchedProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const searchIcon = <FontAwesomeIcon icon={faSearch} />;
 
   useEffect(() => {
     setProducts(PRODUCTS);
   }, []);
-
+  
   useEffect(() => {
-    setFilteredProducts(products.filter((product) =>
+    setSearchedProducts([...filteredProducts].filter((product) =>
       product.title.toLowerCase().includes(searchQuery.toLowerCase())
       )
     );
-  }, [searchQuery, products])
+  }, [searchQuery, filteredProducts])
+  
+  useEffect(() => {
+    const sortArray = type => {
+      const types = {
+        unsorted: 'unsorted',
+        brand: 'brand',
+        title: 'title',
+        price: ['price', 'finalPrice']
+      };
+      const sortProperty = types[type];
+      let sortedArray;
+      
+      if (type === 'brand' || type === 'title') {
+        sortedArray = [...products].sort((a, b) => {
+          if (a[sortProperty] < b[sortProperty]) {
+            return -1;
+          } else if (a[sortProperty] > b[sortProperty]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      } else if (type === 'price') {
+        sortedArray = [...products].sort((a, b) => a[sortProperty[0]][sortProperty[1]] - b[sortProperty[0]][sortProperty[1]]);
+      } else {
+        sortedArray = [...products];
+      }
+      setFilteredProducts(sortedArray);
+    }
+    
+    sortArray(sortType);
+    }, [sortType, products])
 
-  const renderCards = filteredProducts.map((product) => {
+  const selectProduct = (prodObj) => setSelectedProduct(prodObj);
+  const unselectProduct = () => setSelectedProduct(null);
+  
+  const renderCards = searchedProducts.map((product) => {
     return (
-      <ProductCard key={product.id} product={product} />
+      <ProductCard 
+        key={product.id} 
+        product={product} 
+        handleClick={selectProduct}
+        selectedProduct={selectedProduct}
+      />
     );
   });
 
+  const renderSelectedCard = selectedProduct => {
+    if (selectedProduct !== null) {
+      return (
+        <>
+          <h3 className="selected-product__title">{selectedProduct.title}</h3>
+          <ul className="selected-product_prevent-shrink">
+            <ProductCard product={selectedProduct}
+              handleClick={unselectProduct}
+            />
+          </ul>
+        </>
+      )
+    } else {
+      return (
+        <div></div>
+      )
+    }
+  }
 
   return(
     <div className="catalog">
-      <h1 className="catalog__title">Category</h1>
-      <div className="search-wrapper">
-        <input type="text" placeholder="Search" className="search" onChange={(e) => setSearchQuery(e.target.value)}/>
-        <select className="sort" id="sort" name="sort">
-          <option value="" disabled selected hidden>Sort by</option>
-          <option value="brand">Brand (A To Z)</option>
-          {/* <option value="brand-desc">Brand (Z To A)</option> */}
-          <option value="title">Title (A To Z)</option>
-          {/* <option value="title-dsc">Title (Z To A)</option> */}
-          <option value="price">Price (Low To High)</option>
-          {/* <option value="price-desc">Price (High To Low)</option> */}
+      <h1 className="catalog__title">Men's shoes</h1>
+      <div className="search-sort-wrapper">
+        <div className="search-bar">
+          <label htmlFor="search" className="search-icon">{searchIcon}</label>
+          <input type="text" placeholder="Search" id="search" className="search-input" onChange={(e) => setSearchQuery(e.target.value)}/>
+        </div>
+        <select className="sort" id="sort" name="sort" value={sortType} onChange={(e) => setSortType(e.target.value)}>
+          <option value="unsorted" disabled hidden>Sort by</option>
+          <option value="brand">Brand</option>
+          <option value="title">Title</option>
+          <option value="price">Price</option>
         </select>
       </div>
       <ul className="product-grid">
         {renderCards}
-        {filteredProducts.length === 0 
+        {searchedProducts.length === 0 
           && <li className="not-found-msg">
                 No products were found with name <q><strong>{searchQuery}</strong></q>. Check spelling or try other name.
               </li>
         }
       </ul>
-      <div className="selected-product-wrapper">
-        <h3>Selected product</h3>
-        <div className="selected"></div>
+      <div className="selected-product__wrapper">
+        {renderSelectedCard(selectedProduct)}
       </div>
     </div>
   )
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Product.scss';
 
-const Product = ({product}) => {
-  // console.log(product);
+const Product = ({product, handleClick, selectedProduct}) => {
   let curencySymbol = "€";
-
+  
   if (product.price.currency.toUpperCase() !== "EUR") curencySymbol = product.price.currency;
+  
+  const toggleActiveStyle = (id) => {
+    if (selectedProduct !== undefined && selectedProduct !== null && id === selectedProduct.id) {
+      return "product-card product-card_active";
+    } else {
+      return "product-card";
+    }
+  }
 
   return (
-    <li className="product-card">
+    <li className={toggleActiveStyle(product.id)} onClick={() => handleClick(product)} >
       <div className="product-card__image-wrapper">
         <img src={`images/${product.image}`} alt={`${product.title}`}/>
       </div>
